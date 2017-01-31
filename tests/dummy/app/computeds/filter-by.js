3416694023cc2e5242b5043adcaac495e9a8ddd9
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-01-31 13:30:04 +0100

    rename to ember-classy-computeds

--- tests/dummy/app/computeds/filter-by.js
+++ tests/dummy/app/computeds/filter-by.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import ClassBasedComputedProperty from 'ember-class-based-cps';
+import ClassBasedComputedProperty from 'ember-classy-computeds';
 
 const { observer, computed: { filter }, defineProperty } = Ember;
 
