aeb374d2207274b6a2a71ecf1ad82b14e168174d
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-03-26 12:42:57 -0700

    update ember-cli to 2.12.0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,5 @@
-'use strict';
-
 /* eslint-env node */
-
-let EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
+const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
