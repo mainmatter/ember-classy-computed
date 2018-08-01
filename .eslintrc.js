f3d73c6bf0f635fee3311859f9496fd4bea80838
Chris Manson <chris.manson@simplabs.com>
2018-08-01 17:14:03 +0100

    fixing tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,31 @@
 module.exports = {
   root: true,
   extends: 'simplabs',
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  rules: {
+  },
   overrides: [
     // node files
     {
       files: [
         'ember-cli-build.js',
+        'index.js',
         'testem.js',
         'blueprints/*/index.js',
         'config/**/*.js',
-        'lib/*/index.js'
+        'tests/dummy/config/**/*.js'
+      ],
+      excludedFiles: [
+        'addon/**',
+        'addon-test-support/**',
+        'app/**',
+        'tests/dummy/app/**'
       ],
       parserOptions: {
         sourceType: 'script',
