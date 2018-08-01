a89a86a2dcfc8cf6522b498929fa85fc06695edd
Chris Manson <chris.manson@simplabs.com>
2018-08-01 16:59:53 +0100

    using ember-cli-update --reset to bring in line with modern addons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,24 @@
 module.exports = {
   root: true,
   extends: 'simplabs',
+  overrides: [
+    // node files
+    {
+      files: [
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js'
+      ],
+      parserOptions: {
+        sourceType: 'script',
+        ecmaVersion: 2015
+      },
+      env: {
+        browser: false,
+        node: true
+      }
+    }
+  ]
 };
